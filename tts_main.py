0dfb6138481fbfe3805b4160a5b41375f26b5deb
Sourav Kumar Bose <sourav@fold.money>
2024-08-29 15:44:10 +0530

    tts output

--- tts_main.py
+++ tts_main.py
@@ -1,18 +1,14 @@
 import gradio as gr
 from text_to_speech import TextToSpeech
+import numpy as np
 
 text_to_speech = TextToSpeech()
 
 # Define the Gradio interface
 def generate_output(text):
     # Convert search result to speech
-    return_speech_wav = True
-    if return_speech_wav:
-        speech_path = text_to_speech.convert(text, return_speech = return_speech_wav)
-        return open(speech_path, "rb")
-    else:
-        speech = text_to_speech.convert(text, return_speech = return_speech_wav)
-        return speech
+    speech = text_to_speech.convert(text, return_speech = False)
+    return speech["sampling_rate"], np.array(speech["audio"][0])
 
 # Create the Gradio interface
 gr.Interface(fn=generate_output, inputs="text", outputs="audio").launch()
