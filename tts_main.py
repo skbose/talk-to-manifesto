7b6d4306f65d384846b7ba37dff72238874fda65
Varshil Gandhi <varshilgandhi28@gmail.com>
2024-08-29 14:34:54 +0530

    Update tts_main.py

--- tts_main.py
+++ tts_main.py
@@ -1,1 +1,13 @@
+import gradio as gr
+from text_to_speech import TextToSpeech
 
+text_to_speech = TextToSpeech()
+
+# Define the Gradio interface
+def generate_output(text):
+    # Convert search result to speech
+    speech = text_to_speech.convert(text, return_speech = True)
+    return speech
+
+# Create the Gradio interface
+gr.Interface(fn=generate_output, inputs="text", outputs="audio").launch()
