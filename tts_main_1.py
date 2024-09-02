c1d589aa5923cf2b60030bd94003d6d0ae63e22e
Varshil Gandhi <varshilgandhi28@gmail.com>
2024-09-03 02:31:58 +0530

    Update tts_main_1.py

--- tts_main_1.py
+++ tts_main_1.py
@@ -4,13 +4,29 @@ import numpy as np
 
 text_to_speech = TextToSpeech()
 
-# Define the Gradio interface
-def generate_output(text):
-    # Convert search result to speech
-    audio = np.array([])
-    for speech in  text_to_speech.convert_stream(text):
-        audio = np.concatenate([audio, np.array(speech["audio"][0])])
-        yield speech['sampling_rate'], audio
+def stream_audio(text):
+    i = 0
+    for speech in text_to_speech.convert(text):
+        i += 1
+        file = f"{i}.wav"
+        # Convert the audio to 16-bit integer
+        audio_int16 = np.int16(speech["audio"][0] / np.max(np.abs(speech["audio"][0])) * 32767)
+        # Write the WAV file
+        scipy.io.wavfile.write(
+            file,
+            rate=speech["sampling_rate"],
+            data=audio_int16
+        )
+        yield file
+        
 
-# Create the Gradio interface
-gr.Interface(fn=generate_output, inputs="text", outputs=gr.Audio(autoplay=True)).queue().launch()
+with gr.Blocks() as demo:
+    input_text = gr.Textbox(lines=1, label="Convert Marathi Text to Speech - TTS Streaming")
+    stream_as_file_btn = gr.Button("Generate Speech")
+    stream_as_file_output = gr.Audio(streaming=True, elem_id="stream_as_file_output", autoplay=True)
+    stream_as_file_btn.click(
+        stream_audio, input_text, stream_as_file_output
+    )
+
+if __name__ == "__main__":
+    demo.launch()
